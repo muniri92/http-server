1bbabfbedd9f41ebfc2dcbc5a95bfaedf48cdb88
David Flegal <flegal.david@gmail.com>
2016-03-08 17:25:58 -0800

    tests for echo server

--- client.py
+++ client.py
@@ -1,24 +1,27 @@
 # _*_ coding utf-8 _*_
+"""Echo Server file."""
 import socket
 
-infos = socket.getaddrinfo('127.0.0.1', 5000)
-stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
 
+address = ('127.0.0.1', 5000)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-client = socket.socket(*stream_info[:3])
-client.connect(stream_info[-1])
-
-msg = u'this is the message to send.....'
-client.sendall(msg.encode('utf-8'))
+client.connect(address)
 
 
-buffer_length = 8
-message_complete = False
+def get_msg():
+    """Ask user for message to send."""
+    msg = raw_input("what would you like to send? \n >>>>>> ")
+    return msg
 
-while not message_complete:
-    data = client.recv(buffer_length)
-    print(data.decode('utf-8'))
-    if len(data) < buffer_length:
-        break
 
-client.close()
+def send(msg):
+    """Function to send user message and recv echo from server."""
+    client.sendall(msg.encode('utf-8'))
+    data = client.recv(32)
+    client.close()
+    print (data.decode('utf-8'))
+    return (data.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    send(get_msg())
--- server.py
+++ server.py
@@ -1,21 +1,21 @@
 # _*_ coding utf-8 _*_
+"""A."""
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-
 address = ('127.0.0.1', 5000)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 server.bind(address)
 server.listen(1)
 conn, addr = server.accept()
 
-buffer_length = 8
-message_complete = False
+buffer_length = 16
+message_complete = True
 
-while not message_complete:
+while message_complete:
     part = conn.recv(buffer_length)
     print(part.decode('utf-8'))
-    if len(part) < buffer_length:
+    if not part:
         break
-    conn.sendall(part)
+    conn.sendall(part.encode('utf-8'))
 
 conn.close()
