989f068d979a8bdf5dbdd5e5c2a1b0df1e3f6299
muniri92 <mibrah04@gmail.com>
2016-03-13 21:24:54 -0700

    parametized the pytest and cleaned up repo and added to readme

--- src/gevent_server.py
+++ src/gevent_server.py
@@ -3,12 +3,6 @@ import socket
 import io
 import os
 
-# address = ('127.0.0.1', 5000)
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# server.bind(address)
-# server.listen(1)
-# buffer_length = 1024
 server_path = "/Users/admin-1/http_server/http-server/webroot"
 
 
